bfc9a110c80a0f437da20168ec6985748094d015
tanzehao <tanzehao@bytedance.com>
2023-09-08 14:29:51 +0800

    update
    Change-Id: Iaa4afe5c4c764ca4a6afeaf920709b9297620bb9

--- maintest.py
+++ maintest.py
@@ -11,6 +11,7 @@ def maintest(model, enc_input, start_symbol):
     enc_outputs, enc_self_attns = model.Encoder(enc_input)
     dec_input = torch.zeros(1, tgt_len).type_as(enc_input.data)
     next_symbol = start_symbol
+    res = [0] * tgt_len
     for i in range(0, tgt_len):
         dec_input[0][i] = next_symbol
         dec_outputs, _, _ = model.Decoder(dec_input, enc_input, enc_outputs)
@@ -18,7 +19,10 @@ def maintest(model, enc_input, start_symbol):
         prob = projected.squeeze(0).max(dim=-1, keepdim=False)[1]
         next_word = prob.data[i]
         next_symbol = next_word.item()
-    return dec_input
+        res[i] = next_symbol
+    print(res)
+    return res
+
 
 # enc_inputs 对应['我 是 学 生 P'] ['我 喜 欢 学 习'] ['我 是 男 生 P']
 # [1, 2, 3, 4, 0],\n[1, 5, 6, 3, 7],\n[1, 2, 8, 4, 0]
@@ -33,11 +37,7 @@ loader = Data.DataLoader(MyDataSet(enc_inputs, dec_inputs, dec_outputs), 3, True
 enc_inputs, _, _ = next(iter(loader))
 model = torch.load('model.pth')
 predict_dec_input = maintest(model, enc_inputs[0].view(1, -1).cuda(), start_symbol=tgt_vocab["S"])
-predict, _, _, _ = model(enc_inputs[0].view(1, -1).cuda(), predict_dec_input)
-predict1 = predict.data
-predict2 = predict1.max(1, keepdim=True)
-predict3 = predict2[1]
+
 
 print([src_idx2word[int(i)] for i in enc_inputs[0]], '->',
-      [idx2word[n.item()] for n in predict3.squeeze()])
-
+      [idx2word[n] for n in predict_dec_input])
